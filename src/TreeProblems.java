ee584c77831649682e1b48eb3b916405c655eb73
kelley <kelleycastillo@outlook.com>
2026-09-24 13:32:09 -0700

    postOrder done

--- src/TreeProblems.java
+++ src/TreeProblems.java
@@ -27,8 +27,15 @@ public class TreeProblems {
 
    If the root is null, do nothing.
    */
-  public static <T> void postOrder(Node<T> root) {
+  public static <T> void postOrder(Node<T> root) { 
+    if (root == null) {
+      return;
   }
+  for (Node<T> child : root.children) { 
+    postOrder(child);
+  }
+  System.out.println(root.value);
+}
 
   /*
    postOrder (Node Version)
